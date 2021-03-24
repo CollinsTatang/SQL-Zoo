5b6c4e6b2c600de856d6cfba2790672376611288
Collins Tatang <makungongcollins@gmail.com>
2021-03-24 11:55:22 +0100

    Added Nobel select and Noble quiz

--- zoo.sql
+++ zoo.sql
@@ -31,3 +31,28 @@ SELECT population FROM world WHERE name = 'United Kingdom';
 SELECT name, population FROM world WHERE continent IN ('Europe', 'Asia');
 SELECT name FROM world WHERE name IN ('Cuba', 'Togo');
 
+----------------------------------------------------------------------------------------------------------------
+--Select From Nobel
+SELECT yr, subject, winner FROM nobel WHERE yr = 1950 
+SELECT winner FROM nobel WHERE yr = 1962 AND subject = 'Literature'
+SELECT yr, subject FROM nobel WHERE  winner = 'Albert Einstein'
+SELECT winner FROM nobel WHERE yr >= 2000 AND subject = 'Peace'
+SELECT yr, subject, winner FROM nobel WHERE (yr >= 1980 AND yr <= 1989) AND subject = 'Literature'
+SELECT * FROM nobel WHERE winner IN ('Theodore Roosevelt','Woodrow Wilson','Barack Obama','Jimmy Carter')
+SELECT winner FROM nobel WHERE winner LIKE 'John%'
+SELECT * FROM nobel WHERE (yr = 1980 AND subject = 'Physics') OR (yr = 1984 AND subject = 'Chemistry')
+SELECT * FROM nobel WHERE yr = 1980 AND subject  NOT IN('Chemistry', 'Medicine')
+SELECT * FROM nobel WHERE (yr < 1910 AND subject = 'Medicine') OR (yr >= 2004 AND subject = 'Literature')
+SELECT * FROM nobel WHERE winner = 'PETER GRÜNBERG'
+SELECT * FROM nobel WHERE winner = 'EUGENE O\'NEILL' --remove last '
+SELECT winner, yr, subject FROM nobel WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner
+SELECT winner, subject FROM nobel WHERE yr = 1984 ORDER BY subject IN ('Physics', 'Chemistry'), subject, winner
+
+--Nobel Quiz
+SELECT name FROM nobel WHERE winner LIKE 'C%' AND winner LIKE '%n'
+SELECT COUNT(subject) FROM nobel WHERE subject = 'Chemistry' AND yr BETWEEN 1950 and 1960
+SELECT COUNT(DISTINCT yr) FROM nobel WHERE yr NOT IN (SELECT DISTINCT yr FROM nobel WHERE subject = 'Medicine')
+-- Medicine	Sir John Eccles Medicine	Sir Frank Macfarlane Burnet;
+SELECT yr FROM nobel WHERE yr NOT IN(SELECT yr FROM nobel WHERE subject IN ('Chemistry','Physics'))
+SELECT DISTINCT yr FROM nobel WHERE subject='Medicine' AND yr NOT IN(SELECT yr FROM nobel WHERE subject='Literature') AND yr NOT IN (SELECT yr FROM nobel WHERE subject='Peace')
+-- Chemistry	1 Literature	1 Medicine	2 Peace	1 Physics	1
